236924a28f8bbe3bf781bb48e10e24f2ef764d34
Luciano de Franca <lucianodefranca86@gmail.com>
2022-01-04 20:51:24 -0300

    Projeto iniciado, classe entidadebase e classe series

---
+++ classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace temp.series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set;  }
+    }
+}
---
+++ classes/Series.cs
@@ -0,0 +1,19 @@
+namespace temp.series
+{
+    public class Series : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        public Series(int id, Genero genero, string titulo, string descricao, int ano) 
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+    }
+}
