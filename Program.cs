4d3447a1f5444410d41d1aea174d0fe54b4b4884
Luciano de Franca <lucianodefranca86@gmail.com>
2022-01-05 00:37:16 -0300

    Ajustes programa principal

--- Program.cs
+++ Program.cs
@@ -1,13 +1,85 @@
 ﻿using System;
+using temp.series.classes;
 
 namespace temp.series
 {
     class Program
     {
+        static SeriesRepository repository = new SeriesRepository();
         static void Main(string[] args)
         {
+            string opcaoUsuario = ObterOpcaoUsuario();
 
-            Console.WriteLine("Hello World!");
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarSeries();
+                        break;
+                    case "2":
+                        //InserirSeries();
+                        break;
+                    case "3":
+                        //AtualizarSeries();
+                        break;
+                    case "4":
+                        //ExcluirSeries();
+                        break;
+                    case "5":
+                        //VisualizarSeries();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine("Obrigado por utilizar nossos serviços.");
+            Console.ReadLine();
+        }
+
+        private static void ListarSeries() 
+        {
+            Console.WriteLine("Listar séries");
+
+            var lista = repository.FindAll();
+
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenhuma série cadastrada.");
+                return;
+            }
+
+            foreach (var serie in lista)
+            {
+                Console.WriteLine("#ID {0}: - {1}", serie.retornaId(), serie.retornaTitulo());
+            }
+        }
+
+        private static string ObterOpcaoUsuario() 
+        {
+            Console.WriteLine();
+            Console.WriteLine("DIO Séries ao seu dispor!!!");
+            Console.WriteLine("Informe a opção desejada: ");
+
+            Console.WriteLine("1 - Listar séries");
+            Console.WriteLine("2 - Inserir nova série");
+            Console.WriteLine("3 - Atualizar série");
+            Console.WriteLine("4 - Excluir série");
+            Console.WriteLine("5 - Visualizar série");
+            Console.WriteLine("C - Limpar tela");
+            Console.WriteLine("X - sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
