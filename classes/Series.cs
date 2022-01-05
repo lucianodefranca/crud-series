2a57116524000bf9b4bdaed79add8e43e9daeb92
Luciano de Franca <lucianodefranca86@gmail.com>
2022-01-04 21:34:53 -0300

    Classe series finalizada

--- classes/Series.cs
+++ classes/Series.cs
@@ -1,19 +1,49 @@
+using System;
+
 namespace temp.series
 {
     public class Series : EntidadeBase
     {
+        public Series(Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+
+        }
         private Genero Genero { get; set; }
         private string Titulo { get; set; }
         private string Descricao { get; set; }
         private int Ano { get; set; }
 
-        public Series(int id, Genero genero, string titulo, string descricao, int ano) 
+        public Series(int id, Genero genero, string titulo, string descricao, int ano)
         {
             this.Id = id;
             this.Genero = genero;
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Genero" + this.Genero + Environment.NewLine;
+            retorno += "Titulo" + this.Titulo + Environment.NewLine;
+            retorno += "Descrição" + this.Descricao + Environment.NewLine;
+            retorno += "Ano de lançamento" + this.Ano + Environment.NewLine;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId() 
+        {
+            return this.Id;
         }
     }
 }
